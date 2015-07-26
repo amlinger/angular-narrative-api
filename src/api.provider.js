a10d686f06eed085d738a01bd26a4cf673c27e7b
Anton Amlinger <anton.amlinger@gmail.com>
2015-07-26 19:34:41 +0200

    Updated API to work with resource changes, and provided doc-comments for this.

--- src/api.provider.js
+++ src/api.provider.js
@@ -1,59 +1,209 @@
-/*global NarrativeApiProvider, getApi*/
-function NarrativeApiProvider() {
+(function (window, angular, undefined) {
   'use strict';
 
-  this.defaults = {
-    auth : null
-  };
-  var defaults = this.defaults, getApi;
-
-
-
-  getApi = function(Resource, NarrativeAuth) {
-    return function (config) {
-      var api = {};
-
-      config = angular.extend(defaults, config);
-      if (!config.auth) {
-        config.auth = NarrativeAuth();
-      }
-
-      function momentTransformation(moment) {
-        return angular.extend(moment, {
-          positions : Resource
-            .ArrayFactory(config.auth, moment.getPath() + 'positions/')
-            .create(),
-          photos : Resource
-            .ArrayFactory(config.auth, moment.getPath() + 'photos/')
-            .create()
-        });
-      }
-
-      api.moments = Resource.ArrayFactory(config.auth, 'moments/')
-        .itemTransform(momentTransformation)
-        .create();
-
-      api.moment = Resource.ItemFactory(config.auth, 'moments/:uuid')
-        .transform(momentTransformation)
-        .create();
-
-      api.photos = Resource.ArrayFactory(config.auth, 'photos/')
-        .create();
-
-      api.user = Resource.ItemFactory(config.auth, 'users/:uuid/')
-        .create();
-
-      api.me = function (options) {
-        return api.user('me', options);
-      };
-
-      return api;
+  /**
+   * @name constructItem
+   *
+   * @description
+   * Returns a handler for constructing an item object.
+   *
+   * @param  {NrtvItemResource} hook The hook to construct for.
+   * @return {function} A function with the signature(uuid, options).
+   */
+  function constructItem(hook) {
+    return function (uuid, options) {
+      return hook.construct(uuid, options);
     };
-  };
+  }
 
-  getApi.$inject = ['NarrativeResource', 'NarrativeAuth'];
-  this.$get = getApi;
-}
+  /**
+   * @name constructArray
+   *
+   * @description
+   * Returns a handler for constructing an array object.
+   *
+   * @param  {NrtvArrayResource} hook The hook to construct for.
+   * @return {function} A function with the signature(uuid, options).
+   */
+  function constructArray(hook) {
+    return function (options) {
+      return hook.construct(options);
+    };
+  }
 
-angular.module('api.narrative')
-  .provider('NarrativeApi', NarrativeApiProvider);
+  /**
+   * @ngdoc service
+   * @name api.narrative.NarrativeApiProvider
+   * @module api.narrative
+   *
+   * @description
+   * Use `NarrativeApiProvider` to configure the defaults for `NarrativeApi`.
+   */
+  function NarrativeApiProvider() {
+
+    /**
+     * @ngdoc property
+     * @name NarrativeApiProvider.defaults['auth']
+     * @module api.narrative
+     * @propertyOf api.narrative.NarrativeApiProvider
+     * @type {NarrativeAuth}
+     *
+     * @description
+     * The default Auth to use for the instances.
+     */
+    this.defaults = {
+      auth : null
+    };
+    var defaults = this.defaults;
+
+    /**
+     * @ngdoc service
+     * @name api.narrative.NarrativeApi
+     * @module api.narrative
+     * @requires api.narrative.NarrativeItemFactory
+     * @requires api.narrative.NarrativeArrayFactory
+     * @requires api.narrative.NarrativeAuth
+     *
+     * @param {object=} config The name or configuration of the instance to
+     *                         fetch or create.
+     * @description
+     * Returns the Api instance.
+     *
+     * @return {Object} The Auth object representing the created instance.
+     */
+    function getAPI (itemFactory, arrayFactory, auth) {
+
+      return function (config) {
+        var api = {};
+
+        config = angular.extend(defaults, config);
+        if (!config.auth) {
+          config.auth = auth();
+        }
+
+        function momentTransform(moment) {
+          return angular.extend(moment, {
+            positions:
+              arrayFactory(config.auth, moment.path() + 'positions/').construct,
+            photos:
+              arrayFactory(config.auth, moment.path() + 'photos/').construct,
+          });
+        }
+
+        /**
+         * @ngdoc method
+         * @name NarrativeAuth.moments
+         * @module api.narrative
+         * @methodOf api.narrative.NarrativeApi
+         *
+         * @param {object=} options Options for filtrating the results.
+         *
+         * @description
+         * A getter for the config object used to create this instance.
+         *
+         * @return {NrtvArrayResource} The corresponding array object for
+         *                             the moments.
+         */
+        api.moments = constructArray(
+          arrayFactory('moments/', config.auth)
+            .itemTransform(momentTransform));
+
+        /**
+         * @ngdoc method
+         * @name NarrativeAuth.moment
+         * @module api.narrative
+         * @methodOf api.narrative.NarrativeApi
+         *
+         * @param {string=} uuid The corresponding uuid for the resource.
+         * @param {object=} options Options for filtrating the results.
+         *
+         * @description
+         * A getter for the config object used to create this instance.
+         *
+         * @return {NrtvItemResource} The corresponding item object for
+         *                             the moment.
+         */
+        api.moment = constructItem(
+          itemFactory('moments/:uuid', config.auth)
+            .transform(momentTransform));
+
+        /**
+         * @ngdoc method
+         * @name NarrativeAuth.photos
+         * @module api.narrative
+         * @methodOf api.narrative.NarrativeApi
+         *
+         * @param {object=} options Options for filtrating the results.
+         *
+         * @description
+         * A getter for the config object used to create this instance.
+         *
+         * @return {NrtvArrayResource} The corresponding array object for
+         *                             the photos.
+         */
+        api.photos = constructArray(
+          arrayFactory('photos/', config.auth));
+
+        /**
+         * @ngdoc method
+         * @name NarrativeAuth.users
+         * @module api.narrative
+         * @methodOf api.narrative.NarrativeApi
+         *
+         * @param {object=} options Options for filtrating the results.
+         *
+         * @description
+         * A getter for the config object used to create this instance.
+         *
+         * @return {NrtvArrayResource} The corresponding array object for
+         *                             the users.
+         */
+        api.users = constructArray(
+          arrayFactory('users/', config.auth));
+
+        /**
+         * @ngdoc method
+         * @name NarrativeAuth.user
+         * @module api.narrative
+         * @methodOf api.narrative.NarrativeApi
+         *
+         * @param {string=} uuid The corresponding uuid for the resource.
+         * @param {object=} options Options for filtrating the results.
+         *
+         * @description
+         * A getter for the config object used to create this instance.
+         *
+         * @return {NrtvItemResource} The corresponding item object for
+         *                             the user.
+         */
+        api.user = constructItem(
+          itemFactory('users/:uuid/', config.auth));
+
+        /**
+         * @ngdoc method
+         * @name NarrativeAuth.me
+         * @module api.narrative
+         * @methodOf api.narrative.NarrativeApi
+         *
+         * @param {object=} options Options for filtrating the results.
+         *
+         * @description
+         * A getter for the config object used to create this instance.
+         *
+         * @return {NrtvItemResource} The corresponding item object for
+         *                             the user.
+         */
+        api.me = function (options) {
+          return api.user('me', options);
+        };
+
+        return api;
+      };
+    }
+    this.$get = ['NarrativeItemFactory', 'NarrativeArrayFactory',
+      'NarrativeAuth', getAPI];
+  }
+
+  angular.module('api.narrative')
+    .provider('NarrativeApi', NarrativeApiProvider);
+}(window, window.angular));
