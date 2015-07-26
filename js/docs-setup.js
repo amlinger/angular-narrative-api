983a37d07ab71010151ffaf82ec40984e61ab6d5
Anton Amlinger <anton.amlinger@gmail.com>
2015-07-26 19:35:48 +0200

    Included build of docs and resource.

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -1,19 +1,46 @@
 NG_DOCS={
   "sections": {
-    "api": "API Documentation"
+    "reference": "API reference"
   },
   "pages": [
     {
-      "section": "api",
+      "section": "reference",
+      "id": "api.narrative",
+      "shortName": "api.narrative",
+      "type": "overview",
+      "moduleName": "api.narrative",
+      "shortDescription": "Narrative API",
+      "keywords": "ahead angular angular-narrative-api api app clientid clientsecret config container controller cors defaults function http interacting javascript module my-client-id my-client-secret my-root-uri narrative narrativeapi narrativeauthprovider narrativerequestprovider narratives oauthapplication open overview platform proxy redirecturi reference support urls"
+    },
+    {
+      "section": "reference",
+      "id": "api.narrative.NarrativeApi",
+      "shortName": "api.narrative.NarrativeApi",
+      "type": "service",
+      "moduleName": "api.narrative",
+      "shortDescription": "Returns the Api instance.",
+      "keywords": "api array config configuration corresponding create created fetch filtrating getter instance item method moment moments narrative narrativeapi narrativearrayfactory narrativeauth narrativeitemfactory object options photos reference representing resource returns service user users uuid"
+    },
+    {
+      "section": "reference",
+      "id": "api.narrative.NarrativeApiProvider",
+      "shortName": "api.narrative.NarrativeApiProvider",
+      "type": "service",
+      "moduleName": "api.narrative",
+      "shortDescription": "Use NarrativeApiProvider to configure the defaults for NarrativeApi.",
+      "keywords": "api configure default defaults instances narrative narrativeapi narrativeapiprovider property reference service"
+    },
+    {
+      "section": "reference",
       "id": "api.narrative.NarrativeArrayFactory",
       "shortName": "api.narrative.NarrativeArrayFactory",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "Use NarrativeItemFactory for getting a resource specific for an API",
-      "keywords": "$q abort aborted actual additional adds angular-style api applied argument array attach attributes authorization cached callback called calling chain chaining cleans collection config configuring construct constructs created data defaults elements endpoint equivalent essentially exist expected fetched fetches fetching foreach function getter hook initially inserting instance interaction item items itemtransform itemtransform- iterating iteration iterations lazy long looping method methods narrative narrativearrayfactory narrativeitemfactory narrativerequest narratives nextpage object obtaining optional options paginated passed path prevented preventing promise public ready rejected request requests resolved resorce resource response retrieving return returned returns scope segment server service setter signature specific stopping subset supplying swapped top transform transformed transforms- url user uuid wrong"
+      "keywords": "$q abort aborted actual additional adds angular-style api applied argument array attach attributes authorization cached callback called calling chain chaining cleans collection config configuring construct constructs created data defaults elements endpoint equivalent essentially exist expected fetched fetches fetching foreach function getter hook initially inserting instance interaction item items itemtransform itemtransform- iterating iteration iterations lazy long looping method methods narrative narrativearrayfactory narrativeitemfactory narrativerequest narratives nextpage object obtaining optional options paginated passed path prevented preventing promise public ready reference rejected request requests resolved resorce resource response retrieving return returned returns scope segment server service setter signature specific stopping subset supplying swapped top transform transformed transforms- url user uuid wrong"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "api.narrative.NarrativeAuth",
       "shortName": "api.narrative.NarrativeAuth",
       "type": "service",
@@ -22,93 +49,93 @@ NG_DOCS={
       "keywords": "$http $injector $q $rootscope $window access_token active additional api appropriate argument arguments assoiciated attributes auth_required authenticated authentication authenticeted authoritychicken authorization authorized behaviour bunch cache call callback called case changing client code complete config configuration configurations configured contining cool correct create created creates data decided default defauts desired determine emitted event exception existing fails fetch fetched firstly flow getoauthtoken getter grant implemented initializes instance instantiated instantly javascript landing location logged login matter method moving namespace narrative narrativeauth narrativeauthprovider note null oauth oauthauthorizationcode oauthclientcredentials oauthimplicit oauthrefreshtoken object outcome parameters post prevents progress promise provided reason recieve recieved redirect redirects reference referenced reject rejected rejecting rejects remove removel representing request requesting requireauth requiring resolve resolved resolves response return returned secret server service set setter specific stored successful supplied supporting throw token token_type tokenobject true unauth unconfigured url valid var wait waitforauth waiting window"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "api.narrative.NarrativeAuthProvider",
       "shortName": "api.narrative.NarrativeAuthProvider",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "Use NarrativeAuthProvider for changing the default behaviour of",
-      "keywords": "$cachefactory $injector accepts api app application applications attributes authorization authorize bad base behaviour browser cache cachefactory-like cachefactoy care changing client clientid clients clientsecret code configuration configured default defaults defines factory fields flow global grant https individually instance instances instantiation js library moment narrative narrativeauth narrativeauthprovider narrativecache narratives note null oauth oauthapplication oauthroutes object open passing path platform practice property redirect redirecturi reflect request requests required resides resolved routes secret service settings storing string support token url user values verify writing"
+      "keywords": "$cachefactory $injector accepts api app application applications attributes authorization authorize bad base behaviour browser cache cachefactory-like cachefactoy care changing client clientid clients clientsecret code configuration configured default defaults defines factory fields flow global grant https individually instance instances instantiation js library moment narrative narrativeauth narrativeauthprovider narrativecache narratives note null oauth oauthapplication oauthroutes object open passing path platform practice property redirect redirecturi reference reflect request requests required resides resolved routes secret service settings storing string support token url user values verify writing"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "api.narrative.NarrativeCache",
       "shortName": "api.narrative.NarrativeCache",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "Use the NarrativeCache as a factoy for creating a cache with similar",
-      "keywords": "$cachefactory api cache cached created creating destroy drop factoy fall function info key key-value localstorage methods namespace narrative narrativecache newly object options pair persists properties puts references remove removeall removes replacement returns service set size undefined values"
+      "keywords": "$cachefactory api cache cached created creating destroy drop factoy fall function info key key-value localstorage methods namespace narrative narrativecache newly object options pair persists properties puts reference references remove removeall removes replacement returns service set size undefined values"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "api.narrative.NarrativeCache.Cache",
       "shortName": "api.narrative.NarrativeCache.Cache",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "A cache object used to store and retrieve data.",
-      "keywords": "adds alongside api cache cached data entries entry exist function key method narrative narrativecache null object remove removeall removes retrieve service store stored undefined"
+      "keywords": "adds alongside api cache cached data entries entry exist function key method narrative narrativecache null object reference remove removeall removes retrieve service store stored undefined"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "api.narrative.NarrativeCacheProvider",
       "shortName": "api.narrative.NarrativeCacheProvider",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "You can use NarrativeCacheProvider as a factory for creating named",
-      "keywords": "$cachefactory angulars api caches creating factory fallback named narrative narrativecacheprovider service"
+      "keywords": "$cachefactory angulars api caches creating factory fallback named narrative narrativecacheprovider reference service"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "api.narrative.NarrativeItemFactory",
       "shortName": "api.narrative.NarrativeItemFactory",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "Use NarrativeItemFactory for getting a resource specific for an API",
-      "keywords": "$q _constructfromobject actual adds angular-style api applied argument attach attributes authorization builds cached called calling chain chaining cleans config configuring construct constructed constructs created data defaults expected fetched fetching getter hook identifier inserting instance interaction internally item method methods narrative narrativeitemfactory narrativerequest narratives object optional options parameter passed path pointing promise public ready rejected relative replacing requests resolved resorce resource retrieving return returns root scope segment server service setter specific string subset supplying swapped top transform transformed transforms- unique url user uuid wrong"
+      "keywords": "$q _constructfromobject actual adds angular-style api applied argument attach attributes authorization builds cached called calling chain chaining cleans config configuring construct constructed constructs created data defaults expected fetched fetching getter hook identifier inserting instance interaction internally item method methods narrative narrativeitemfactory narrativerequest narratives object optional options parameter passed path pointing promise public ready reference rejected relative replacing requests resolved resorce resource retrieving return returns root scope segment server service setter specific string subset supplying swapped top transform transformed transforms- unique url user uuid wrong"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "api.narrative.NarrativeRequestProvider",
       "shortName": "api.narrative.NarrativeRequestProvider",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "NarrativeRequestProvider provides a request method that can be used",
-      "keywords": "api fetching method narrative narrativerequestprovider narratives open platform request resources service"
+      "keywords": "api fetching method narrative narrativerequestprovider narratives open platform reference request resources service"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "NarrativeParamSerializer",
       "shortName": "NarrativeParamSerializer",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "When called, it serializes the provided parameters provided according to",
-      "keywords": "api arrays called converted format function html json module narrative narrativeparamserializer param parameters params provided serializer serializes service string turn url-friendly"
+      "keywords": "api arrays called converted format function html json module narrative narrativeparamserializer param parameters params provided reference serializer serializes service string turn url-friendly"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "NarrativeParamSerializerProvider",
       "shortName": "NarrativeParamSerializerProvider",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "NarrativeParamSerializerProvider provides a serializer for URL",
-      "keywords": "api narrative narrativeparamserializerprovider narratives open parameters passed platform serializer service url"
+      "keywords": "api narrative narrativeparamserializerprovider narratives open parameters passed platform reference serializer service url"
     },
     {
-      "section": "api",
+      "section": "reference",
       "id": "NarrativeRequest",
       "shortName": "NarrativeRequest",
       "type": "service",
       "moduleName": "api.narrative",
       "shortDescription": "A method for making authorized requests with parameters to Narratives",
-      "keywords": "$http api authorized function html making method module narrative narrativerequest narratives open param parameters platform requests serializer service url"
+      "keywords": "$http api authorized function html making method module narrative narrativerequest narratives open param parameters platform reference requests serializer service url"
     }
   ],
   "apis": {
-    "api": true
+    "reference": true
   },
   "html5Mode": false,
   "editExample": true,
-  "startPage": "/api",
+  "startPage": "/reference",
   "scripts": [
     "angular.min.js"
   ]
