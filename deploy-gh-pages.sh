19523a48dbe90f98bbfcb5d07c269fff7168e0ae
Anton Amlinger <anton.amlinger@gmail.com>
2015-08-06 13:21:51 +0200

    Scipt bug fixes after Travis build.

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -10,12 +10,16 @@ if [ -z "$(git status --porcelain $DOC_FOLDER)" ]; then
   exit 0
 else
   echo "$DOC_FOLDER needs updating..."
+
+  git config user.email "anton.amlinger@gmail.com"
+  git config user.name "Anton Amlinger"
+
   BRANCH="$(git name-rev --name-only HEAD)"
   git checkout -b temp-gh-branch
   git add $DOC_FOLDER && git commit -m "Updated documentation"
   git subtree push --prefix $DOC_FOLDER "https://${GH_TOKEN}@${GH_REF}" origin temp-gh-branch:gh-pages
   git checkout $BRANCH
-  ggit branch -D temp-gh-branch
+  git branch -D temp-gh-branch
 
   exit 0
 fi
