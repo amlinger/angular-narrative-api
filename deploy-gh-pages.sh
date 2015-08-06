f398ebaa88ddcec1afb0eea89d8c0c82c6bc70a1
Anton Amlinger <anton.amlinger@gmail.com>
2015-08-06 13:33:46 +0200

    Script additional printing.

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -17,6 +17,10 @@ else
   BRANCH="$(git name-rev --name-only HEAD)"
   git checkout -b temp-gh-branch
   git add $DOC_FOLDER && git commit -m "Updated documentation"
+
+  echo "Pushing to:"
+  echo "https://${GH_TOKEN}@${GH_REF}"
+  
   git subtree push --prefix $DOC_FOLDER "https://${GH_TOKEN}@${GH_REF}" origin temp-gh-branch:gh-pages
   git checkout $BRANCH
   git branch -D temp-gh-branch
